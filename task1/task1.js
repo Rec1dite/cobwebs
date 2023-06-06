008d999e13e14b5dae9ec56b2774eda548696d4b
Rec1dite <rec1dite@gmail.com>
2023-06-07 00:19:49 +0200

    Fixed FPS drop on file reselect; Added wireframe & rotation toggles and sample step selector

--- task1/task1.js
+++ task1/task1.js
@@ -14,9 +14,8 @@ var imgData = null;
 var near = -1;
 var far = 3;
 var radius = 1.0;
-var theta = 0.0;
-var phi = 1.0;
-var dr = 5.0 * Math.PI / 180.0;
+var theta = 90;
+var phi = 0;
 
 var isRot = true;
 var isWire = false;
@@ -25,8 +24,8 @@ var rX = 0.0;
 var rY = 0.0;
 var rZ = 0.0;
 
-var left = -1.0;
-var right = 1.0;
+var left = -1.5;
+var right = 1.5;
 var ytop = 1.0;
 var bottom = -1.0;
 
@@ -37,12 +36,14 @@ var camera;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
+var sampleStep = 1.0;
+
 function getPixIndex(x, z, sampleStep, width) {
     return 4 * (x * sampleStep + z * sampleStep * width);
 }
 
 function constructPlane() {
-    const sampleStep = 1.0;
+    numVertices = 0;
     if (imgData == null) { return; }
 
     // Reset lists
@@ -59,13 +60,13 @@ function constructPlane() {
     for (let x = 0; x < w; x++) {
 
         // Draw forward strip
-        for (let z = 0; z < h; z++) {
+        for (let z = 0; z < h-1; z++) {
 
             // Get pixel color
             let pixIndex = getPixIndex(x, z, sampleStep, imgData.width);
             let r = imgData.data[pixIndex] / 255.0;
-            let g = imgData.data[pixIndex+1] / 255.0;
-            let b = imgData.data[pixIndex+2] / 255.0;
+            // let g = imgData.data[pixIndex+1] / 255.0;
+            // let b = imgData.data[pixIndex+2] / 255.0;
 
             let xp = (x+1) % w;
             let zp = (z+1) % h;
@@ -76,44 +77,39 @@ function constructPlane() {
             // let r, g, b;
             // r = g = b = x / w;
 
-            addSquare((x-w/2), (z-h/2), y1, y2, y3, y4, 50*scale, scale, r, g, b);
+            addSquare((x-w/2), (z-h/2), y1, y2, y3, y4, 50*scale/sampleStep, scale);
         }
     }
 }
 
-function addSquare(x, z, y1, y2, y3, y4, yscale, scale, r, g, b) {
+function addSquare(x, z, y1, y2, y3, y4, yscale, scale) {
     const sx = scale*x;
     const sz = scale*z;
 
-    verts.push(sx, yscale*y1, sz, 1);
-    cols.push(r, g, b, 1);
-    norms.push(0, 1, 0);
-    numVertices += 1;
+    let col1 = [1, 0, 0];
+    let col2 = [0, 0, 1];
 
+    // Triangle 1
+    verts.push(sx, yscale*y1, sz, 1);
     verts.push(sx, yscale*y2, sz+scale, 1);
-    cols.push(r, g, b, 1);
-    norms.push(0, 1, 0);
-    numVertices += 1;
-
     verts.push(sx+scale, yscale*y3, sz+scale, 1);
-    cols.push(r, g, b, 1);
-    norms.push(0, 1, 0);
-    numVertices += 1;
 
+    for (let i = 0; i < 3; i++) {
+        cols.push(col1[0], col1[1], col1[2], 1);
+        norms.push(0, 1, 0);
+    }
+
+    // Triangle 2
     verts.push(sx, yscale*y1, sz, 1);
-    cols.push(r, g, b, 1);
-    norms.push(0, 1, 0);
-    numVertices += 1;
-
     verts.push(sx+scale, yscale*y3, sz+scale, 1);
-    cols.push(r, g, b, 1);
-    norms.push(0, 1, 0);
-    numVertices += 1;
-
     verts.push(sx+scale, yscale*y4, sz, 1);
-    cols.push(r, g, b, 1);
-    norms.push(0, 1, 0);
-    numVertices += 1;
+
+    for (let i = 0; i < 3; i++) {
+        cols.push(col2[0], col2[1], col2[2], 1);
+        norms.push(0, 1, 0);
+    }
+
+    numVertices += 6;
 }
 
 var nBuffer;
@@ -122,6 +118,30 @@ var cBuffer;
 var gl;
 var program;
 
+var loader;
+var start;
+var showLoader = false;
+function runLoader(timestamp) {
+    if (start == undefined) {
+        start = timestamp;
+        loader.style.display = "block";
+    }
+    const elapsed = timestamp - start;
+
+    const progress = Math.log(elapsed / 1000 + 1);
+
+    loader.style.width = `${progress * 100}%`;
+
+    // Stop condition
+    if (progress >= 10) {
+        showLoader = false;
+        start = undefined;
+    }
+
+    if (showLoader) {
+        window.requestAnimationFrame(runLoader);
+    }
+}
 
 window.onload = function init() {
     //===== SETUP =====//
@@ -131,11 +151,9 @@ window.onload = function init() {
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.depthRange(0.8, 1.0);
     gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.PRIMITIVE_RESTART_FIXED_INDEX)
 
     gl.clearColor(0.12, 0.12, 0.18, 1.0);
-
-    theta = 10;
+    gl.clearDepth(1)
 
     //===== LOAD SHADERS =====//
     //  Load shaders and initialize attribute buffers
@@ -145,15 +163,21 @@ window.onload = function init() {
     // reloadCanvas();
 
     //===== HANDLE FILE UPLOAD =====//
+    loader = document.getElementById("loader");
+    let previewCanvas = document.getElementById('previewCanvas');
+    let context = previewCanvas.getContext('2d');
+
     document.getElementById('fileInput').addEventListener('change', function(e) {
         let img = new Image();
         img.src = URL.createObjectURL(e.target.files[0]);
 
+        // Start loader animation
+        showLoader = true;
+        runLoader();
+
         img.onload = function() {
-            let previewCanvas = document.getElementById('previewCanvas');
             previewCanvas.width = img.width;
             previewCanvas.height = img.height;
-            let context = previewCanvas.getContext('2d');
             context.drawImage(img, 0, 0, img.width, img.height);
             imgData = context.getImageData(0, 0, previewCanvas.width, previewCanvas.height);
 
@@ -164,65 +188,75 @@ window.onload = function init() {
 
     }, false);
 
+    //===== ADD EVENT LISTENERS =====//
+    document.getElementById("mapRes").value = sampleStep;
+
+    let toggleWire = document.getElementById("toggleWire")
+    toggleWire.addEventListener("click", () => {
+        isWire = !isWire;
+        toggleWire.innerHTML = isWire ? "Disable Wireframe" : "Enable Wireframe";
+    });
+
+    let toggleRot = document.getElementById("toggleRot");
+    toggleRot.addEventListener("click", () => {
+        isRot = !isRot;
+        toggleRot.innerHTML = isRot ? "Disable Rotation" : "Enable Rotation";
+    });
+
+    let mapRes = document.getElementById("mapRes");
+    mapRes.addEventListener("change", e => {
+        sampleStep = e.target.value;
+    });
 }
 
+function setupGLAttrib(buffer, arrData, attribName, chunkSize) {
+    buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(arrData), gl.STATIC_DRAW);
+
+    var attrib = gl.getAttribLocation(program, attribName);
+    gl.vertexAttribPointer(attrib, chunkSize, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(attrib);
+}
+
+var renderId = 0;
 function reloadCanvas() {
     //===== DELETE PREVIOUS BUFFERS =====//
     gl.deleteBuffer(nBuffer);
     gl.deleteBuffer(cBuffer);
     gl.deleteBuffer(vBuffer);
 
+
     //===== CREATE GEOMETRY ======//
     constructPlane();
 
     //===== CREATE BUFFERS =====//
     // normals
-    nBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(norms), gl.STATIC_DRAW);
 
-    var vNormal = gl.getAttribLocation(program, "vNormal");
-    gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vNormal);
-
-    // colors
-    cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(cols), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
-
-    // vertices
-    vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(verts), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+    // setupGLAttrib(nBuffer, norms, "vNormal", 3);
+    setupGLAttrib(cBuffer, cols, "vColor", 4);
+    setupGLAttrib(vBuffer, verts, "vPosition", 4);
 
     transMatLoc = gl.getUniformLocation(program, "transMatrix");
     viewMatLoc = gl.getUniformLocation(program, "viewMatrix");
     projMatLoc = gl.getUniformLocation(program, "projMatrix");
 
-
-    let wire = document.getElementById("wire");
-    isWire = wire.checked;
-    wire.onchange = () => {
-        isWire = wire.checked;
-    }
-    render();
-
+    render(++renderId);
+    showLoader = false;
+    loader.style.display = "none";
 }
 
 // See [https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Animating_objects_with_WebGL]
-var render = () => {
+let lastRender = 0;
+var render = (id) => {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    phi += 0.01;
-    theta += 0.01;
+    let now = Date.now();
+    if (isRot) {
+        phi += (now - lastRender) * 0.001;
+        // theta += 0.01;
+    }
+    lastRender = now;
 
     camera = vec3(radius * Math.sin(phi), radius * Math.sin(theta),
         radius * Math.cos(phi));
@@ -243,12 +277,19 @@ var render = () => {
     // Possible options:
     // POINTS, LINES, LINE_STRIP, LINE_LOOP, TRIANGLES, TRIANGLE_STRIP, TRIANGLE_FAN
 
-    let drawMode = gl.TRIANGLES;
+    let drawMode = isWire ? gl.LINE_LOOP : gl.TRIANGLES;
 
-    for (let i = 0; i < numVertices; i += 6) {
-        gl.drawArrays(drawMode, i, 6);
+    if (isWire) {
+        for (let i = 0; i < numVertices; i += 6) {
+            gl.drawArrays(drawMode, i, 6);
+        }
     }
-    // gl.drawArrays(drawMode, 0, numVertices)
+    else {
+        gl.drawArrays(drawMode, 0, numVertices)
+    }
 
-    requestAnimFrame(render);
+    // Stop condition
+    if (id == renderId) {
+        window.requestAnimationFrame(() => render(id));
+    }
 }
